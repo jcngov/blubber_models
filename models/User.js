eccfa6d2c7a73437e9c0a28adac861d7799866c2
Jerry Ngov <jcngov@yahoo.com>
2016-03-03 15:57:45 -0800

    clean up User model file

--- models/User.js
+++ models/User.js
@@ -1,7 +1,5 @@
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/blubber_app');
-
 // SCHEMA
 var userSchema = new mongoose.Schema({
   name:      { type: String, required: true },
@@ -13,19 +11,4 @@ var userSchema = new mongoose.Schema({
 // MODEL
 var User = mongoose.model("User", userSchema);
 
-User.remove({}, function(err, results){
-  User.create([
-    {name: "John Marshall",             email: "jm@email.com", moderator: true},
-    {name: "Oliver Wendell Holmes Jr.", email: "ow@email.com"},
-    {name: "Thurgood Marshall",         email: "tm@email.com"},
-    {name: "Sandra Day O'Connor",       email: "sd@email.com"}
-    ], function(err, users){
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(users);
-      }
-  mongoose.connection.close();
-    })
-})
-
+module.exports = User;
