42b07aa8e96293bdbe89ab4756756dab1c354e0b
Jerry Ngov <jcngov@yahoo.com>
2016-03-03 16:54:39 -0800

    rewrite seeds.js

--- seeds.js
+++ seeds.js
@@ -2,30 +2,33 @@ var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/blubber_app');
 
-// SCHEMA
-var userSchema = new mongoose.Schema({
-  name:      { type: String, required: true },
-  email:     { type: String, required: true },
-  // password: String,
-  moderator: { type: Boolean, default: false }
-});
+var User = require('./models/User'),
+    Thread = require('./models/Thread');
 
-// MODEL
-var User = mongoose.model("User", userSchema);
+Thread.remove({}, function(err, thread) {
+  User.remove({}, function(err, results){
+    User.create([
+      {name: "John Marshall",             email: "jm@email.com", moderator: true},
+      {name: "Oliver Wendell Holmes Jr.", email: "ow@email.com"},
+      {name: "Thurgood Marshall",         email: "tm@email.com"},
+      {name: "Sandra Day O'Connor",       email: "sd@email.com"}
+      ], function(err, users){
+        if (err) {
+          console.log(err);
+        } else {
+          console.log(users);
+          var john = users[0];
+        }
 
-User.remove({}, function(err, results){
-  User.create([
-    {name: "John Marshall",             email: "jm@email.com", moderator: true},
-    {name: "Oliver Wendell Holmes Jr.", email: "ow@email.com"},
-    {name: "Thurgood Marshall",         email: "tm@email.com"},
-    {name: "Sandra Day O'Connor",       email: "sd@email.com"}
-    ], function(err, users){
-      if (err) {
-        console.log(err);
-      } else {
-        console.log(users);
-      }
-  mongoose.connection.close();
-    })
+        // create threads
+
+        Thread.create(
+          {name: "YOLO", creator: john},
+          function(err, thread) {
+            if (err) console.log(err);
+            console.log(thread);
+        mongoose.connection.close();
+        })
+      });
+  });
 })
-
